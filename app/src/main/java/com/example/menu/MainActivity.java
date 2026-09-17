ab69a13bc32575431cffdd8bf7ec00fd480fbdfc
sehyunf <ssam010925@gmail.com>
2026-09-17 11:31:02 +0900

    feat: dialog and toase 추가

--- app/src/main/java/com/example/menu/MainActivity.java
+++ app/src/main/java/com/example/menu/MainActivity.java
@@ -1,60 +1,66 @@
 package com.example.menu;
 
-import android.graphics.Color;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
-    LinearLayout baseLayout;
+public class MainActivity extends Activity {
+    TextView tvName, tvEmail;
     Button button1;
+    EditText dlgEdtName, dlgEdtEmail;
+    TextView toastText;
+    View dialogView, toastView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("사용자 정보 입력");
 
-        setTitle("배경색 바꾸기");
-
-        baseLayout = (LinearLayout) findViewById(R.id.baseLayout);
+        tvName = (TextView) findViewById(R.id.tvName);
+        tvEmail = (TextView) findViewById(R.id.tvEmail);
         button1 = (Button) findViewById(R.id.button1);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        MenuInflater mInflater = getMenuInflater();
-        mInflater.inflate(R.menu.menu1, menu);
-        return true;
-    }
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dialogView = (View) View.inflate(MainActivity.this,
+                        R.layout.dialog1, null);
+                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                dlg.setTitle("사용자 정보 입력");
+                dlg.setView(dialogView);
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int itemId = item.getItemId();
+                dlg.setPositiveButton("확인",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dlgEdtName = (EditText) dialogView.findViewById(R.id.dlgEdt1);
+                                dlgEdtEmail = (EditText) dialogView.findViewById(R.id.dlgEdt2);
 
-        if (itemId == R.id.itemR) {
-            baseLayout.setBackgroundColor(Color.RED);
-            return true;
-        } else if (itemId == R.id.itemG) {
-            baseLayout.setBackgroundColor(Color.GREEN);
-            return true;
-        } else if (itemId == R.id.itemB) {
-            baseLayout.setBackgroundColor(Color.BLUE);
-            return true;
-        } else if (itemId == R.id.subRotate) {
-            button1.setRotation(45);
-            return true;
-        } else if (itemId == R.id.subSize) {
-            button1.setScaleX(2);
-            return true;
-        }
+                                tvName.setText(dlgEdtName.getText().toString());
+                                tvEmail.setText(dlgEdtEmail.getText().toString());
+                            }
+                        });
 
-        return super.onOptionsItemSelected(item);
+                dlg.setNegativeButton("취소",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast toast = new Toast(MainActivity.this);
+                                toastView = (View) View.inflate(MainActivity.this,
+                                        R.layout.toast1, null);
+                                toastText = (TextView) toastView.findViewById(R.id.toastText1);
+                                toastText.setText("취소했습니다");
+                                toast.setView(toastView);
+                                toast.show();
+                            }
+                        });
+
+                dlg.show();
+            }
+        });
     }
 }
